111f773380c29152995497873fab636f14ab5550
ddhee <kidakitaki@gmail.com>
2018-03-18 16:35:53 +0800

    All tests fixed

--- Test/test.m
+++ Test/test.m
@@ -1,24 +1,20 @@
-clear
-close all
 %% Use global rotation angles and transformation vectors
-n=3
+clear
+n=11;
 load('RotTranM.mat');
 im1 = imread('data\Fr1.png');
 MergeMap= rgb2gray(im1);
-%     tmp = RotTran(1:3,2);
-%     R = [cos(tmp(1)),-sin(tmp(1));sin(tmp(1)),cos(tmp(1))];
-%     t = tmp(2:3);
-%     im2 = imread(join(['data\Fr',num2str(3),'.png'])); 
-%     Dmap= rgb2gray(im2);
-%     MergeMap = merging(MergeMap,Dmap,R,t);
-
+t0 = [0;0];
+t_offset = [0;0];
 for i = 2:n
     tmp = RotTran(1:3,i-1);
     R = [cos(tmp(1)),-sin(tmp(1));sin(tmp(1)),cos(tmp(1))];
     t = tmp(2:3);
     im2 = imread(join(['data\Fr',num2str(i),'.png'])); 
     Dmap= rgb2gray(im2);
-    MergeMap = merging(MergeMap,Dmap,R,t); 
+    t0 = t0 + t_offset([2 1],:);
+    t = t + t0;
+    [MergeMap,t_offset] = merging(MergeMap,Dmap,R,t); 
 end
 figure('Name','global rotation angles and transformation vectors');
 imshow(MergeMap)
--- Test/test2.m
+++ Test/test2.m
@@ -1,18 +1,21 @@
 %% Use relative motions i to i-1
 clear;
-n=3;
+n=11;
 load('RelativeMotions.mat');
 im1 = imread(join(['data\Fr',num2str(n),'.png'])); 
 MergeMap= rgb2gray(im1);
-
+% t0 = [0;0];
+% t_offset = [0;0];
 for k = 1:n-1
     i = n-k;     % i = n-1 ~ 1
     m = cell2mat(RelativeMotions(i,i+1));
     R = [m(1,1),m(1,2);m(2,1),m(2,2)];
     t = [m(1,3);m(2,3)];
     im2 = imread(join(['data\Fr',num2str(i),'.png'])); 
-    Dmap= rgb2gray(im2);
-    MergeMap = merging(Dmap,MergeMap,R,t); 
+%     Dmap= rgb2gray(im2);
+%     t0 = t0 + t_offset([2 1],:);
+%     t = t + t0;
+    [MergeMap,t_offset] = merging(Dmap,MergeMap,R,t); 
 end
 figure('Name','relative motions i to i-1');
 imshow(MergeMap)
